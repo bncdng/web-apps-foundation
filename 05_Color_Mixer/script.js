6e4ac8569171121e22da895c6c01555810708f49
Bianca Dang <bncdng@gmail.com>
2024-04-08 09:21:29 +0200

    add coding task todo app

---
+++ 05_Color_Mixer/script.js
@@ -0,0 +1,4 @@
+const redSlider = document.getElementById("rangeSlider_R");
+const greenSlider = document.getElementById("rangeSlider_G");
+const blueSlider = document.getElementById("rangeSlider_B");
+const mixedColor = document.getElementById("colorDisplay");
