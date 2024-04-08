ae416a4544ba18fa3b07d1c562074e6a07852f05
Bianca Dang <bncdng@gmail.com>
2024-04-08 12:11:58 +0200

    update color mixer

--- 05_Color_Mixer/script.js
+++ 05_Color_Mixer/script.js
@@ -1,4 +1,47 @@
-const redSlider = document.getElementById("rangeSlider_R");
-const greenSlider = document.getElementById("rangeSlider_G");
-const blueSlider = document.getElementById("rangeSlider_B");
-const mixedColor = document.getElementById("colorDisplay");
+const red = document.getElementById("red");
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const colorDisplay = document.getElementById("colorDisplay");
+
+function render() {
+  const r = Number(redStringValue);
+  const g = Number(greenStringValue);
+  const b = Number(blueStringValue);
+
+  // 1. Hintergrund einfärben
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+  // 2. Hex Wert darstellen
+  let redHex = r.toString(16);
+  let greenHex = g.toString(16);
+  let blueHex = b.toString(16);
+
+  if (redHex.length == 1) {
+    redHex = "0" + redHex;
+  }
+
+  if (greenHex.length == 1) {
+    greenHex = "0" + greenHex;
+  }
+
+  if (blueHex.length == 1) {
+    blueHex = "0" + blueHex;
+  }
+
+  colorHex.textContent = `#${redHex}${greenHex}${blueHex}`;
+}
+
+render();
+
+// Event Listener
+red.addEventListener("input", () => {
+  render();
+});
+
+green.addEventListener("input", () => {
+  render();
+});
+
+blue.addEventListener("input", () => {
+  render();
+});
